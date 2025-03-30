d692c35dab5a88514f1eb2cf68bb33f5e14ebc72
Conner Yule <conneryule@gmail.com>
2025-03-30 02:14:14 -0400

    simi working game

--- app/src/main/java/org/midterm/Game.java
+++ app/src/main/java/org/midterm/Game.java
@@ -1,1 +1,32 @@
 package org.midterm;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import com.github.javafaker.Faker;
+
+public class Game {
+
+	public void gameLoop() {
+
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		Random rand = new Random();
+		Faker faker = new Faker();
+
+		String teamOne = faker.team().name();
+		String teamTwo = faker.team().name();
+
+		System.out.println(teamOne);
+		System.out.println(teamTwo);
+
+		map.put(teamOne, rand.nextInt(31));
+		map.put(teamTwo, rand.nextInt(31));
+
+		for (Map.Entry<String, Integer> entry : map.entrySet()) {
+			System.out.println("Team: " + entry.getKey() + " " + "Score: " + entry.getValue());
+
+		}
+
+	}
+
+}
